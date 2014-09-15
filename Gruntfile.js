e964a0d25be4825d9061f29dff8e4215ad30c265
James Allardice <jim.allardice@gmail.com>
2014-09-15 15:41:00 +0100

    Added JSHint to Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,19 @@
 module.exports = function (grunt) {
 
+  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-karma');
 
   grunt.initConfig({
+    jshint: {
+      options: {
+        jshintrc: true
+      },
+      all: [
+        'Gruntfile.js',
+        'src/**/*.js',
+        'test/**/*.js'
+      ]
+    },
     karma: {
       unit: {
         options: {
@@ -27,4 +38,9 @@ module.exports = function (grunt) {
       }
     }
   });
+
+  grunt.registerTask('default', [
+    'jshint',
+    'karma'
+  ]);
 };
