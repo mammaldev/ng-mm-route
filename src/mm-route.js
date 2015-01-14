4b899acd89c6b07c05fc8dbf43378872c48fdd65
Reetta Vaahtoranta <reetta.vaahtoranta@mammal.io>
2015-01-14 10:05:50 +0000

    mm-route working with new route structure but without role validation

--- src/mm-route.js
+++ src/mm-route.js
@@ -7,41 +7,6 @@ angular.module('mmRoute', [
   var interpolationMarkers = /[:*?]/g;
   var interpolationOptional = /:[^\/]+\?/g;
 
-  // Utility function to add route definitions to the $routeProvider from the
-  // ngRoute module
-  function applyRoutes(routes) {
-    Object.keys(routes).forEach(function (base) {
-
-      var baseRoute = routes[base];
-
-      if (baseRoute.hasOwnProperty('url')) {
-
-        // If this is an actual route definition then add it to the
-        // $routeProvider
-        $routeProvider.when(baseRoute.url.split('?')[0], baseRoute);
-      } else {
-
-        // Otherwise it's a nested definition and we need to recurse into it
-        applyRoutes(baseRoute);
-      }
-    });
-  }
-
-  // Utility function to return the value of a nested object key when given a
-  // string such as 'key.nested.deeply'
-  function mapKeys(keys, obj) {
-    if (!keys.length) {
-      return obj;
-    }
-
-    var key = keys[0];
-
-    if (!obj.hasOwnProperty(key)) {
-      return;
-    }
-
-    return mapKeys(keys.slice(1, keys.length), obj[key]);
-  }
 
   //
   // Set a URL to redirect to when no other route matches the current URL
@@ -68,13 +33,13 @@ angular.module('mmRoute', [
   };
 
   //
-  // Set the roles available to the active user
+  // Sets a function to retrieve roles  available to the active user
   //
   // Arguments:
-  //   roles    {String[]}    List of roles available to the active user
+  //   roleGetter    {function()}    List of roles available to the active user
   //
-  this.setRoles = function setRoles(roles) {
-    this.roles = roles;
+  this.setRoleGetter = function setRoleGetter( roleGetter ) {
+    this.roleGetter = roleGetter;
   };
 
   //
@@ -83,35 +48,55 @@ angular.module('mmRoute', [
   // Arguments:
   //   routes    {Object}    Map of roles to route definitions
   //
-  this.setRoutes = function setRoutes(routes) {
+  this.setRoutes = function setRoutes( routes ) {
+
+    function parseUrls( urlObj ) {
+
+      if ( urlObj.hasOwnProperty('url') ) {
+        var configObj = {
+          url: urlObj.url,
+        };
+        if ( urlObj.access.length > 1 ) {
+          configObj.routeConf = {
+            template: '<body><h1>MANY</h1><role-resolver route=\'' + JSON.stringify(urlObj) + '\'></role-resolver></body>',
+          };
+        } else {
+          configObj.routeConf = urlObj.access[0].page;
+        }
+        urlObjects.push(configObj);
+      } else {
+        var objectKeys = Object.keys(urlObj);
+        objectKeys.forEach(function ( key ) {
+          parseUrls(urlObj[key]);
+        });
+      }
+    }
 
     // mmRoute provides role-based routing. There's no point using it if you
     // haven't set up the roles first
-    if (!this.roles) {
+    if ( !this.roleGetter ) {
       throw new Error(
-        'No roles have been configured. Call setRoles before setRoutes'
+        'Role getter has not yet been configured. Call setRoles before setRoutes'
       );
     }
 
     this.routes = routes;
+    var urlObjects = [];
+    parseUrls(routes);
 
     // Set up a route via $routeProvider for each route available to the given
     // role
-    this.roles.forEach(function (role) {
 
-      var roleRoutes = routes[role];
-
-      if (roleRoutes) {
-        applyRoutes(roleRoutes);
-      }
+    urlObjects.forEach(function ( urlObject ) {
+      $routeProvider.when(urlObject.url.split('?')[0], urlObject.routeConf);
     });
+
   };
 
   // Set up the mmRoute factory
   this.$get = [ '$location', function mmRouteFactory($location) {
 
     var routes = this.routes;
-    var roles = this.roles;
 
     //
     // Get a URL by route name and role. Route paramater data will be
@@ -120,25 +105,17 @@ angular.module('mmRoute', [
     // Arguments:
     //   name      {String}    Name of the route; nested names separated by .
     //   [data]    {Object}    A map of route paramater names to data
-    //   [role]    {String}    The role to use in the case of multiple routes
-    //
-    function get(name, data, role) {
-      /*jshint devel: true */
 
-      // The `data` and `role` arguments are both optional. This handles the
-      // case where `data` is undefined but `role` is not
-      if (arguments.length === 2 && typeof data === 'string') {
-        role = data;
-        data = undefined;
-      }
+    function get( name, data ) {
+    /*jshint devel: true */
 
       // Get the matching route definition
-      var route = getRoute(name, role);
+      var route = getRoute(name);
 
       // If we haven't found a matching route by now then either it doesn't
       // exist or the user does not have a role that is able to access it. In
       // either case we cannot proceed
-      if (!route) {
+      if ( !route ) {
         return;
       }
 
@@ -181,36 +158,22 @@ angular.module('mmRoute', [
     }
 
     //
-    // Get a route definition by name and role.
+    // Get a route definition by name.
     //
     // Arguments:
     //   name      {String}    Name of the route; nested names separated by .
-    //   [role]    {String}    The role to use in the case of multiple routes
     //
-    function getRoute(name, role) {
+
+    function getRoute( name ) {
 
       var keys = name.split('.');
-      var route;
 
-      if (role) {
+      var urlObject = routes;
+      keys.forEach(function ( key ) {
+        urlObject = urlObject[key];
+      });
 
-        // If a role is specified we want to return the route corresponding to
-        // that role (if it exists)
-        return mapKeys(keys, routes[role]);
-
-      } else {
-
-        // If a role was not specified we return the first matching route for
-        // the roles available to the active user
-        for (var i = 0; i < roles.length; i++) {
-          route = mapKeys(keys, routes[roles[i]]);
-          if (route) {
-            return route;
-          }
-        }
-      }
-
-      return route;
+      return urlObject;
     }
 
     //
@@ -218,10 +181,10 @@ angular.module('mmRoute', [
     //
     // Arguments:
     //   name      {String}    Name of the route; nested names separated by .
-    //   [role]    {String}    The role to use in the case of multiple routes
     //
-    function goTo(name, role) {
-      $location.url(get(name, role));
+
+    function goTo( name ) {
+      $location.url(get(name));
     }
 
     return {
